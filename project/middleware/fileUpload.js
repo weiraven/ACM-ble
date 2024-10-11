cf440fccd69f07d582245d43ccc426b3c4810762
Raven Wei <weiwardraven@gmail.com>
2024-10-11 01:15:07 -0400

    started a middleware directory to contain fileUpload

---
+++ project/middleware/fileUpload.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './public/img')
+    },
+    filename: (req, file, cb) => {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    const mimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
+
+    if (mimeTypes.includes(file.mimetype)) {
+        return cb(null, true);
+    } else {
+        cb(new Error('Invalid file type: only jpg, png, and gif file types allowed'));
+    }
+}
+
+const upload = multer({
+    storage: storage,
+    limits: { fileSize: 2*1024*1024 },
+    fileFilter: fileFilter
+}).single('image');
+
+exports.fileUpload = (req, res, next) => {
+    upload(req, res, err => {
+        if (err instanceof multer.MulterError) {
+            err.status = 400;
+            next(err);
+        } else if (err) {
+            err.status = 400;
+            next(err);
+        } else {
+            next();
+        }
+    });
+}
