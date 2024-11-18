16ce15ea86316d0a7a115335372ddc85aceb5a53
Raven Wei <weiwardraven@gmail.com>
2024-11-17 21:09:46 -0500

    corrected redundant logic in async/await refactor of userController implementation (since demo video used traditional promise structure)

--- acm-events/controllers/userController.js
+++ acm-events/controllers/userController.js
@@ -1,20 +1,19 @@
 const User = require('../models/user');
+const Event = require('../models/event');
 
-// New user controls
+// Render new user sign-up page
 exports.showSignUp = (req, res) => {
     res.render('user/new');
 };
 
+// Create a new user
 exports.createUser = async (req, res, next) => {
     try {
-
         let user = new User(req.body);
         await user.save();
         req.flash('success', 'New account created successfully!');
         res.redirect('/users/login');
-
     } catch(err) {
-
         if(err.name === 'ValidationError') {
             req.flash('error', err.message);
             return res.redirect('/users/new');
@@ -29,77 +28,72 @@ exports.createUser = async (req, res, next) => {
     }
 };
 
-// Login controls
+// Render login page
 exports.showLogin = (req, res) => {
     res.render('user/login');
 };
 
+// User login controls
 exports.loginUser = async (req, res, next) => {
     try {
-
         let email = req.body.email;
         let password = req.body.password;
-        let user = await User.findOne({email: email});
+        let user = await User.findOne({ email: email });
 
-        if(user) {
-
-            let result = await user.comparePassword(password);
-
-            try {
-                if(result) {
-                    req.session.user = user._id;
-                    req.flash('success', 'You have successfully logged in.');
-                    res.redirect('/users/profile');
-
-                } else {
-                    req.flash('error', 'Password is incorrect.');
-                    res.redirect('/users/login');
-                }
-
-            } catch(err) {
-                next(err);
-            }
-
-        } else {
+        if(!user) {
             req.flash('error', 'Email address does not exist.');
-            res.redirect('/users/login');
+            return res.redirect('/users/login');
         }
 
+        let isUser = await user.comparePassword(password);
+
+        if(isUser) {
+            req.session.user = user._id;
+            req.flash('success', 'You have sucessfully logged in.');
+            res.redirect('/users/profile');
+        } else {
+            req.flash('error', 'Password is incorrect.');
+            res.redirect('/users/login');
+        }
     } catch(err) {
         next(err);
     }
 };
 
-// Profile controls
+// User profile controls
 exports.profile = async (req, res, next) => {
-    let id = req.session.user;
-
-    if(!id) {
-        return res.render('user/profile', {user: null});
-    }
-
-    let user = await User.findById(id);
-
     try {
-        res.render('user/profile', {user});
+        let userId = req.session.user;
+        if (!userId) {
+            req.flash('error', 'Please log in to view your profile.');
+            return res.redirect('/users/login');
+        }
+
+        // fetch user and user-created events simultaneously
+        let [user, events] = await Promise.all([
+            User.findById(userId),
+            Event.find({ host: userId }) // remember to refactor Event model to hold user ID in 'host' field
+        ]);
+        res.render('user/profile', { user, events });
     } catch(err) {
         next(err);
     }
 };
 
 // Logout controls
-exports.logout = async (req, res, next) => {
-    // check if session exists
-    if(!req.session.user) {
+exports.logout = (req, res, next) => {
+    // check for active session
+    if (!req.session.user) {
         req.flash('error', 'No user is currently logged in.');
-        return res.redirect('/users/login'); // redirect to display flash
+        return res.redirect('/users/login');
     }
-
+    
+    // destroy session and handle log out
     req.session.destroy(err => {
         if(err) {
             return next(err);
-        } else {
-            res.redirect('/users/login');
         }
+        req.flash('success', 'You have successfully logged out.');
+        res.redirect('/users/login');
     });
 };
