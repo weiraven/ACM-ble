efb50bd4ea4a80490a4f57ea896c018fe67f5382
Raven Wei <twei1@uncc.edu>
2024-11-19 15:16:08 -0500

    updated event routes to use auth and validation middleware

--- acm-events/routes/eventRoutes.js
+++ acm-events/routes/eventRoutes.js
@@ -1,30 +1,33 @@
 // required modules
 const express = require('express');
-const router = express.Router();
 const {fileUpload} = require('../middleware/fileUpload');
+const {isLoggedIn, isHost} = require('../middleware/auth');
+const {validateEventId} = require('../middleware/valdiator')
 
 // import controller
 const controller = require('../controllers/eventController');
+
+const router = express.Router();
 
 // GET /events: send all events to the user
 router.get('/', controller.index);
 
 // GET /events/new: send html form for creating a new event
-router.get('/newEvent', controller.newEvent);
+router.get('/newEvent', isLoggedIn, controller.newEvent);
 
 // POST /events: create a new event
-router.post('/', fileUpload, controller.create);
+router.post('/', isLoggedIn, fileUpload, controller.create);
 
 // GET /events/:id: send details of event identified by id
-router.get('/:id', controller.show);
+router.get('/:id', validateEventId, controller.show);
 
 // GET /events/:id/edit: send html form for editing an existing event
-router.get('/:id/edit', controller.edit);
+router.get('/:id/edit', validateEventId, isLoggedIn, isHost, controller.edit);
 
 // PUT /events/:id: update the event identified by id
-router.put('/:id', fileUpload, controller.update);
+router.put('/:id', validateEventId, isLoggedIn, isHost, fileUpload, controller.update);
 
 // DELETE /events/:id, delete the event identified by id
-router.delete('/:id', controller.delete);
+router.delete('/:id', validateEventId, isLoggedIn, isHost, controller.delete);
 
 module.exports = router;
