b0fad085ea6fcc7927af27bf7c7e3beb5b42ffb0
Raven Wei <weiwardraven@gmail.com>
2024-11-22 20:40:52 -0500

    removed password validation check from createUser function and further streamlined coding logic

--- acm-events/controllers/userController.js
+++ acm-events/controllers/userController.js
@@ -16,15 +16,12 @@ exports.createUser = async (req, res, next) => {
     } catch(err) {
         if(err.name === 'ValidationError') {
             req.flash('error', err.message);
-            return res.redirect('/users/new');
-        }
-
-        if(err.code === 11000) {
+        } else if(err.code === 11000) {
             req.flash('error', 'An account with this email address already exists.');
-            return res.redirect('/users/new');
+        } else {    
+           return next(err);
         }
-
-        next(err);
+        return res.redirect('/users/new');
     }
 };
 
@@ -46,15 +43,14 @@ exports.loginUser = async (req, res, next) => {
         }
 
         let isUser = await user.comparePassword(password);
-
-        if(isUser) {
-            req.session.user = user._id;
-            req.flash('success', 'You have sucessfully logged in.');
-            res.redirect('/users/profile');
-        } else {
+        if(!isUser) {
             req.flash('error', 'Password is incorrect.');
             res.redirect('/users/login');
         }
+
+        req.session.user = user._id;
+        req.flash('success', 'You have sucessfully logged in.');
+        res.redirect('/');
     } catch(err) {
         next(err);
     }
@@ -64,15 +60,11 @@ exports.loginUser = async (req, res, next) => {
 exports.profile = async (req, res, next) => {
     try {
         let userId = req.session.user;
-        if (!userId) {
-            req.flash('error', 'Please log in to view your profile.');
-            return res.redirect('/users/login');
-        }
 
-        // fetch user and user-created events simultaneously
+        // Fetch user and user-created events simultaneously
         let [user, events] = await Promise.all([
             User.findById(userId),
-            Event.find({ host: userId }) // remember to refactor Event model to hold user ID in 'host' field
+            Event.find({ hostname: userId })
         ]);
         res.render('user/profile', { user, events });
     } catch(err) {
@@ -82,18 +74,14 @@ exports.profile = async (req, res, next) => {
 
 // Logout controls
 exports.logout = (req, res, next) => {
-    // check for active session
-    if (!req.session.user) {
-        req.flash('error', 'No user is currently logged in.');
-        return res.redirect('/users/login');
-    }
-    
-    // destroy session and handle log out
+    // Store flash message before session destruction 
+    req.flash('success', 'You have successfully logged out.');
+
+    // Destroy session to log user out
     req.session.destroy(err => {
         if(err) {
             return next(err);
         }
-        req.flash('success', 'You have successfully logged out.');
         res.redirect('/users/login');
     });
 };
