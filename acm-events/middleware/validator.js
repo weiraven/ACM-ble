67839f196180ec9fe61a3f502dd6a9bf3d840b90
Raven Wei <weiwardraven@gmail.com>
2024-11-22 20:37:09 -0500

    added validatePassword function to further declutter user controller functions

--- acm-events/middleware/validator.js
+++ acm-events/middleware/validator.js
@@ -7,3 +7,16 @@ exports.validateEventId = (req, res, next) => {
     }
     next();
 };
+
+exports.validatePassword = (req, res, next) => {
+    const { password, confirmPassword } = req.body;
+    if (!password || password.length < 8) {
+        req.flash('error', 'Password must be at least 8 characters.');
+        return res.redirect('/users/new');
+    }
+    if (password !== confirmPassword) {
+        req.flash('error', 'Passwords do not match.');
+        return res.redirect('/users/new');
+    }
+    next();
+};
