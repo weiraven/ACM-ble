9ae5e17a9bcec7472c6497e0fbf8428ac7b4ce84
Raven Wei <weiwardraven@gmail.com>
2024-12-01 01:53:30 -0500

    added showRSVPPopup and hideRSVPPopup functions to handle RSVP modal functionality on the eventDetails view

--- acm-events/public/js/common.js
+++ acm-events/public/js/common.js
@@ -1,3 +1,26 @@
+const event = require("../../models/event");
+
 function confirmDelete() {
     return confirm('Are you sure you want to delete this event? This action cannot be undone.');
 }
+
+function showRSVPPopup(event) {
+    event.preventDefault();
+    const popup = document.getElementById('rsvp-popup');
+    popup.classList.remove('hidden');
+    popup.style.display = 'flex';
+
+}
+
+function hideRSVPPopup() {
+    const popup = document.getElementById('rsvp-popup');
+    popup.classList.add('hidden');
+    popup.style.display = 'none';
+}
+
+document.addEventListener('click', function (event) {
+    const popup = document.getElementById('rsvp-popup');
+    if (event.target === popup) {
+        hideRSVPPopup(); // hide pop-up if user clicks outside of it
+    }
+});
